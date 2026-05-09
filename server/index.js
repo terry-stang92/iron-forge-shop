ce4e07f87b881565e1a2e047f29bba5e8182186b
Fernando Terrazas <portonesaluminorb@gmail.com>
2026-05-09 07:50:36 -0500

    fix port for render

--- server/index.js
+++ server/index.js
@@ -22,5 +22,5 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err))
 
-  app.listen(5000, () => console.log('Server running on port 5000'))
-
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
